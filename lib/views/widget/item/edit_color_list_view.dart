0b5576e30ea16dc126b5a3a89bd9a8a6eccec779
baselhegazi <baselmohammed226@gmail.com>
2024-07-30 06:14:00 +0300

    create edit color list view

--- lib/views/widget/item/edit_color_list_view.dart
+++ lib/views/widget/item/edit_color_list_view.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app1/cubits/add_cubit/add_note_cubit.dart';
 
+import '../../../models/note_model.dart';
 import '../helper/constant.dart';
 import 'color_item.dart';
 
-class ColorListView extends StatefulWidget {
-  const ColorListView({super.key});
-
+class EditColorListView extends StatefulWidget {
+  const EditColorListView({super.key, required this.noteModel});
+  final NoteModel noteModel;
   @override
-  State<ColorListView> createState() => _ColorListViewState();
+  State<EditColorListView> createState() => _EditColorListViewState();
 }
 
-class _ColorListViewState extends State<ColorListView> {
-  int currentItem = 0;
+class _EditColorListViewState extends State<EditColorListView> {
+  late int colorIndex;
+  @override
+  void initState() {
+    colorIndex = kColors.indexOf(Color(widget.noteModel.color));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -26,12 +30,12 @@ class _ColorListViewState extends State<ColorListView> {
             ),
             child: ColorItem(
               onPressed: () {
-                currentItem = index;
-                BlocProvider.of<AddNoteCubit>(context).color = kColors[index];
+                colorIndex = index;
+                widget.noteModel.color = kColors[index].value;
                 setState(() {});
               },
               color: kColors[index].value,
-              isActive: currentItem == index,
+              isActive: colorIndex == index,
             ),
           );
         },
